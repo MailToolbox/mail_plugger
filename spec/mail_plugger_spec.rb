426b96173478c2207ffe6b53ce210d36be1ac55c
Norbert Szivos <sysqa@yahoo.com>
2020-11-23 07:13:14 +0100

    Add freeze to the version and chnage tests

--- spec/mail_plugger_spec.rb
+++ spec/mail_plugger_spec.rb
@@ -1,9 +1,3 @@
 RSpec.describe MailPlugger do
-  it "has a version number" do
-    expect(MailPlugger::VERSION).not_to be nil
-  end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
