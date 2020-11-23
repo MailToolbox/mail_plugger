a3910335f590cdb404e831d10681183645a90d3b
Norbert Szivos <sysqa@yahoo.com>
2020-11-23 07:19:39 +0100

    Use WebMock in tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 require "bundler/setup"
 require "mail_plugger"
+require "webmock/rspec"
+
+WebMock.disable_net_connect!(allow_localhost: true)
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
