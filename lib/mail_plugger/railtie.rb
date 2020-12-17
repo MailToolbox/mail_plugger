9bd2a703788631b1e03c5366031a70ec244eb061
Norbert Szivos <sysqa@yahoo.com>
2020-12-17 08:03:05 +0100

    Add missing Railtie file

---
+++ lib/mail_plugger/railtie.rb
@@ -0,0 +1,12 @@
+module MailPlugger
+  class Railtie < Rails::Railtie
+    initializer 'mail_plugger.add_delivery_method' do
+      ActiveSupport.on_load :action_mailer do
+        ActionMailer::Base.add_delivery_method(
+          :mail_plugger,
+          MailPlugger::DeliveryMethod
+        )
+      end
+    end
+  end
+end
