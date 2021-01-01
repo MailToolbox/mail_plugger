61562b4b306fcd9155d31c78aeaae399d6807230
Norbert Szivos <sysqa@yahoo.com>
2021-01-01 16:58:07 +0100

    Change the order of the error classes

--- lib/mail_plugger/error.rb
+++ lib/mail_plugger/error.rb
@@ -2,24 +2,24 @@
 
 module MailPlugger
   class Error < StandardError
-    # Specific error class for errors if tries to add undelclared option
-    # in plug_in block
-    class WrongPlugInOption < Error; end
-
-    # Specific error class for errors if delivery system is not given
-    class WrongDeliverySystem < Error; end
+    # Specific error class for errors if client is not given or has a wrong type
+    class WrongApiClient < Error; end
 
     # Specific error class for errors if delivery options is not given or
     # has a wrong type
     class WrongDeliveryOptions < Error; end
 
-    # Specific error class for errors if client is not given or has a wrong type
-    class WrongApiClient < Error; end
+    # Specific error class for errors if delivery settings has a wrong type
+    class WrongDeliverySettings < Error; end
+
+    # Specific error class for errors if delivery system is not given
+    class WrongDeliverySystem < Error; end
 
     # Specific error class for errors if parameter is not given
     class WrongParameter < Error; end
 
-    # Specific error class for errors if delivery settings has a wrong type
-    class WrongDeliverySettings < Error; end
+    # Specific error class for errors if tries to add undelclared option
+    # in plug_in block
+    class WrongPlugInOption < Error; end
   end
 end
