bef2a33ec5f125951a9221d541f8a6920ed9c35a
Norbert Szivos <sysqa@yahoo.com>
2021-01-25 05:39:15 +0100

    Add a really early version of MailGrabber
    This is just an experimental version...

--- lib/mail_plugger.rb
+++ lib/mail_plugger.rb
@@ -11,6 +11,12 @@ require 'fake_plugger/delivery_method'
 # If we are using this gem outside of Rails then do not load this code.
 require 'fake_plugger/railtie' if defined?(Rails)
 
+require 'mail_grabber/error'
+require 'mail_grabber/database_helper'
+require 'mail_grabber/delivery_method'
+# If we are using this gem outside of Rails then do not load this code.
+require 'mail_grabber/railtie' if defined?(Rails)
+
 module MailPlugger
   class << self
     attr_reader :client, :delivery_options, :delivery_settings
